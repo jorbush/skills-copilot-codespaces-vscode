f133b1ea17c8f4977b57918974b0dd476e3ce070
Jordi <55429631+jorbush@users.noreply.github.com>
2023-11-01 18:50:49 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,16 +1,7 @@
 // Create a web server
 
-const http = require('http');
+// Import the express module
+const express = require('express');
 
-const hostname = '127.0.0.1';
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello, World!\n');
-});
-
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-});
+// Create an express application
+const app = express();
